c13b010342517c0ea0de31873b56a1b7e006ca65
adramelech <adramelech.psd@gmail.com>
2021-09-21 15:00:58 +0530

    first logic

--- src/App.js
+++ src/App.js
@@ -1,58 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { LADDERS, SNAKES } from "./snakeAndLadder";
+
+let timeout;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const [position, setPosition] = useState({});
+	const [noOfPlayers, setNoOfPlayers] = useState(0);
+	const [isGameStarted, setIsGameStarted] = useState(false);
+	const [currentPlayer, setCurrentPlayer] = useState(null);
+	const [players, setPlayers] = useState({});
+
+	const setScore = diceValue => {
+		setPlayers({ ...players, [currentPlayer.id]: currentPlayer.score + diceValue });
+	};
+
+	const throwDice = () => {
+		let diceValue = Math.floor(Math.random() * 6 + 1);
+		if (isGameStarted) {
+			setScore(diceValue);
+		}
+	};
+
+	const updateCurrentPlayerScore = () => {
+		setCurrentPlayer({ ...currentPlayer, score: players[currentPlayer.id] });
+	};
+
+	const checkForLadderAndSnake = () => {
+		console.log("ladder");
+		let a;
+		setCurrentPlayer(b => {
+			a = b;
+			return b;
+		});
+		console.log(a);
+		if (LADDERS.hasOwnProperty(currentPlayer.score)) {
+			setScore(LADDERS[currentPlayer.score] - currentPlayer.score);
+		} else if (SNAKES.hasOwnProperty(currentPlayer.score)) {
+			setScore(currentPlayer.score - SNAKES[currentPlayer.score]);
+		}
+	};
+
+	useEffect(() => {
+		if (isGameStarted && currentPlayer) {
+			for (let i in players) {
+				updatePosition(i);
+			}
+		}
+	}, [players, currentPlayer]);
+
+	useEffect(() => {
+		if (isGameStarted && currentPlayer) {
+			updateCurrentPlayerScore();
+		}
+	}, [players]);
+
+	useEffect(() => {
+		if (isGameStarted && currentPlayer) {
+			console.log("asd");
+			setTimeout(checkForLadderAndSnake, 500);
+		}
+	}, [currentPlayer]);
+
+	const updatePosition = player => {
+		let { top, left } = document.querySelector(`#a_${players[player]}`).getBoundingClientRect();
+		setPosition(position => ({
+			...position,
+			[player]: { top, left },
+		}));
+	};
+
+	const generateCells = num => {
+		let array = [];
+		while (num > 0) {
+			array.push(
+				<div className="cell" id={`a_${num}`}>
+					{num}
+				</div>
+			);
+			num--;
+		}
+		return array;
+	};
+
+	const createPlayers = e => {
+		e.preventDefault();
+		if (!isGameStarted) {
+			setNoOfPlayers(e.target[0].value);
+			for (let i = 0; i < e.target[0].value; i++) {
+				setPlayers(players => ({ ...players, [`player_${i}`]: 1 }));
+			}
+		}
+		setIsGameStarted(true);
+	};
+
+	const changePlayer = () => {
+		setCurrentPlayer(currentPlayer => {
+			let id = +currentPlayer?.id.split("_")[1] + 1;
+			let nextPlayer = players[`player_${id}`] ? `player_${id}` : `player_0`;
+			let score = players[nextPlayer];
+			return { id: nextPlayer, score };
+		});
+	};
+
+	useEffect(() => {
+		clearTimeout(timeout);
+		if (Object.keys(players).length > 0) {
+			timeout = setTimeout(changePlayer, 1000);
+		}
+	}, [players]);
+
+	return (
+		<>
+			<div className="app">
+				{generateCells(100)}
+				{Object.keys(players).map(key => (
+					<div className="coin" style={position[key]}></div>
+				))}
+			</div>
+			<form onSubmit={createPlayers}>
+				<input type="number" min="2" max="4" defaultValue="2" />
+				<button type="submit">start</button>
+			</form>
+			<button onClick={throwDice}>CLick ME</button>
+			{/* <h1>{score}</h1> */}
+		</>
+	);
 }
 
 export default App;
