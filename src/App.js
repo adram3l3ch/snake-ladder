ce5fa42ec413a471bc78dd43c4a6b5db02f602e9
adramelech <adramelech.psd@gmail.com>
2021-09-22 18:23:04 +0530

    logic completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { LADDERS, SNAKES } from "./snakeAndLadder";
+import boardImage from "./assets/board.jpg";
 
 let timeout;
 
@@ -7,6 +8,7 @@ function App() {
 	const [position, setPosition] = useState([]);
 	const [isGameStarted, setIsGameStarted] = useState(false);
 	const [currentPlayer, setCurrentPlayer] = useState(null);
+	const [diceValue, setDiceValue] = useState(null);
 	const [score, setScore] = useState([]);
 	const [winner, setWinner] = useState([]);
 
@@ -37,18 +39,22 @@ function App() {
 	};
 
 	const checkForWin = () => {
-		if (score[currentPlayer] === 100) setWinner([...winner, currentPlayer]);
+		if (score[currentPlayer] === 100 && !winner.includes(currentPlayer)) {
+			setWinner([...winner, currentPlayer]);
+			setScore([...score.slice(0, currentPlayer), ...score.slice(currentPlayer + 1)]);
+		}
 	};
 
 	useEffect(() => {
-		if (winner.length + 1 === score.length) {
+		if (winner.length + 1 === position.length) {
 			setIsGameStarted(false);
-			document.write("game over");
+			document.write("game over", winner);
 		}
 	}, [winner]);
 
 	const throwDice = () => {
 		let diceValue = Math.floor(Math.random() * 6 + 1);
+		setDiceValue(diceValue);
 		// let diceValue = 3;
 		if (isGameStarted) {
 			updateScore(diceValue);
@@ -57,7 +63,7 @@ function App() {
 
 	useEffect(() => {
 		setCurrentPlayer(currentPlayer => {
-			if (checkForLadderAndSnake()) return currentPlayer;
+			if (checkForLadderAndSnake() || diceValue === 6) return currentPlayer;
 			else return score[currentPlayer + 1] ? currentPlayer + 1 : 0;
 		});
 		score.map((item, index) => updatePosition(item, index));
@@ -83,15 +89,15 @@ function App() {
 		]);
 	};
 
-	const generateCells = num => {
+	const generateCells = (start, end) => {
 		let array = [];
-		while (num > 0) {
+		while (end >= start) {
 			array.push(
-				<div className="cell" id={`a_${num}`}>
-					{num}
+				<div className="cell" id={`a_${start}`}>
+					{start}
 				</div>
 			);
-			num--;
+			start++;
 		}
 		return array;
 	};
@@ -99,16 +105,27 @@ function App() {
 	return (
 		<>
 			<div className="app">
-				{generateCells(100)}
+				<div className="row">{generateCells(1, 10)}</div>
+				<div className="row">{generateCells(11, 20)}</div>
+				<div className="row">{generateCells(21, 30)}</div>
+				<div className="row">{generateCells(31, 40)}</div>
+				<div className="row">{generateCells(41, 50)}</div>
+				<div className="row">{generateCells(51, 60)}</div>
+				<div className="row">{generateCells(61, 70)}</div>
+				<div className="row">{generateCells(71, 80)}</div>
+				<div className="row">{generateCells(81, 90)}</div>
+				<div className="row">{generateCells(91, 100)}</div>
 				{position.map(value => (
 					<div className="coin" style={value}></div>
 				))}
+				<img src={boardImage} alt="" />
 			</div>
 			<form onSubmit={createPlayers}>
 				<input type="number" min="2" max="4" defaultValue="2" />
 				<button type="submit">start</button>
 			</form>
 			<button onClick={throwDice}>CLick ME</button>
+			<h3>{diceValue}</h3>
 			{/* <h1>{score}</h1> */}
 		</>
 	);
